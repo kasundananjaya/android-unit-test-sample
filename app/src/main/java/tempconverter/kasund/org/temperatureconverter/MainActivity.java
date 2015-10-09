193d13440fad5851af40c3533898593f05ac96b4
Kasun Delgolla <kasun.mck@gmail.com>
2015-10-09 19:51:14 +0530

    Initial commit

---
+++ app/src/main/java/tempconverter/kasund/org/temperatureconverter/MainActivity.java
@@ -0,0 +1,72 @@
+package tempconverter.kasund.org.temperatureconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (EditText) findViewById(R.id.inputValue);
+
+    }
+
+    // this method is called at button click because we assigned the name to the
+    // "OnClick" property of the button
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+                RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+                if (text.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number",
+                                   Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float inputValue = Float.parseFloat(text.getText().toString());
+                if (celsiusButton.isChecked()) {
+                    text.setText(String
+                                         .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+                    celsiusButton.setChecked(false);
+                    fahrenheitButton.setChecked(true);
+                } else {
+                    text.setText(String
+                                         .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+                    fahrenheitButton.setChecked(false);
+                    celsiusButton.setChecked(true);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
